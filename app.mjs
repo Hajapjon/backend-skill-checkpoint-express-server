2d09558c07bbbbbdf6aaf17d462ca1aa19a97493
nichalada <nichaladasuphachayanon@gmail.com>
2025-04-15 23:15:47 +0700

    feat(answer): add voting endpoint for answer by ID

--- app.mjs
+++ app.mjs
@@ -1,11 +1,13 @@
 import express from "express";
 import { questionRouter } from "./routes/question.route.mjs";
+import { answersRouter } from "./routes/answers.route.mjs";
 
 const app = express();
 const port = 4000;
 
 app.use(express.json());
 app.use("/questions",questionRouter)
+app.use("/answers", answersRouter)
 
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
